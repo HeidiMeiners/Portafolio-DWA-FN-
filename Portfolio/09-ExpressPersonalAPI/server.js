a56d7466cf4f816430f5d8359f0fcdc1b6787e3c
Heidi Meiners <0266159@up.edu.mx>
2024-10-30 01:23:23 -0600

    until 9

--- Portfolio/09-ExpressPersonalAPI/server.js
+++ Portfolio/09-ExpressPersonalAPI/server.js
@@ -1,18 +1,59 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public', 'html')); 
 
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname + "/html/index.html");
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+var names = [];
+var tasks = [];
+
+app.get('/', (req, res) => {
+    res.render('index', { names, error: null }); 
 });
 
-app.get("/greet", (req, res) => { 
+app.get('/greet', (req, res) => {
     const name = req.query.name;
-    console.log("Received name:", name);
-    res.send(`Hello, ${name}!`);
+    if (!name) {
+        return res.render('index', { names, error: 'Name is required!' });
+    }
+    names.push(name);
+    res.render('index', { names, error: null });
+});
+
+app.get('/wazzup', (req, res) => {
+    const name = req.query.name; 
+    res.render('wazzup', { name }); 
+});
+
+app.post('/task', (req, res) => {
+    const task = req.body.task;
+    if (task) {
+        tasks.push(task); 
+    }
+    res.render('index', { names, tasks, error: null }); 
+});
+
+app.get('/task', (req, res) => {
+    res.json(tasks); 
+});
+
+app.post('/task/delete', (req, res) => {
+    const taskIndex = parseInt(req.body.index); 
+    if (!isNaN(taskIndex)) {
+        tasks.splice(taskIndex, 1); 
+    }
+    res.render('index', { names, tasks, error: null }); 
+});
+
+app.put('/greet/:name', (req, res) => {
+    const newName = req.params.name;
+    names.push(newName); 
+    res.json(names); 
 });
 
 app.listen(3000, () => {
-    console.log("Server listening on port 3000");
+    console.log(`Server is running on http://localhost:3000`);
 });
